dc7a95e51d89827e5aa0c291d4356f0b4aeb8a8d
Paul Hawxby <phawxby@gmail.com>
2015-04-09 00:57:49 +0100

    Refactoring! Still not perfect but much better!

--- Submission TOC Builder/Builder.cs
+++ Submission TOC Builder/Builder.cs
@@ -1,29 +1,25 @@
-﻿using iTextSharp.text;
-using iTextSharp.text.html.simpleparser;
-using iTextSharp.text.pdf;
-using iTextSharp.tool.xml;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
-using System.Drawing;
 using System.IO;
 using System.Linq;
 using System.Text;
-using System.Threading;
-using System.Threading.Tasks;
 using System.Windows.Forms;
-using System.Xml;
 
 namespace Submission_TOC_Builder
 {
     public partial class Builder : Form
     {
+        private Builders.IBuilder _builder;
+
         public Builder()
         {
             InitializeComponent();
 
-
+            // Should the need arise to build different types of TOC's or anything else we can change
+            // this to a select box some such
+            _builder = new Builders.NeesToc();
         }
 
         private void btn_SelectDirectory_Click(object sender, EventArgs e)
@@ -37,9 +33,8 @@ namespace Submission_TOC_Builder
 
                 dlg.ShowDialog();
 
-                var dir = new DirectoryInfo(dlg.SelectedPath);
-
-                if (dir.Exists)
+                _builder.SetDir(dlg.SelectedPath);
+                if (_builder.ValidDir)
                 {
                     txt_Path.Text = dlg.SelectedPath;
 
@@ -54,14 +49,9 @@ namespace Submission_TOC_Builder
 
         private void updateFoundLbl()
         {
-            if (!string.IsNullOrEmpty(txt_Path.Text))
+            if (_builder.ValidDir)
             {
-                var dir = new DirectoryInfo(txt_Path.Text);
-
-                if (dir.Exists)
-                {
-                    lbl_Found.Text = String.Format("Found {0} pdfs", GetValidPdfsInDirectory(dir, SearchOption.AllDirectories).Count());
-                }
+                    lbl_Found.Text = String.Format("Found {0} pdfs", _builder.FoundFiles);
             }
             else
             {
@@ -76,280 +66,14 @@ namespace Submission_TOC_Builder
 
         private void Build()
         {
-            var dir = new DirectoryInfo(txt_Path.Text);
+            _builder.Build();
 
-            foreach (var subdir in dir.GetDirectories())
-            {
-                BuildPdfForDir(subdir, false);
-            }
-
-            BuildPdfForDir(dir, true);
             lbl_Found.Text = "Done!";
         }
 
         private void txt_Path_TextChanged(object sender, EventArgs e)
         {
-            btn_Build.Enabled = (!String.IsNullOrWhiteSpace(txt_Path.Text) && Directory.Exists(txt_Path.Text));
-        }
-
-        private List<FileInfo> GetValidPdfsInDirectory(DirectoryInfo dir, SearchOption searchOption)
-        {
-            var returnLst = new List<FileInfo>();
-
-            // Check we have a directory and that it exists
-            if (dir.Exists)
-            {
-                // Loop each pdf file in the directory
-                foreach (var file in dir.GetFiles("*.pdf", searchOption))
-                {
-                    // Get the depth
-                    var depth = GetFileDepthFromDir(file, dir);
-
-                    // If the file depth is less that <= 1 and it contains toc then it is a toc. We obviously don't want to include
-                    // that again
-                    if (depth <= 1 && file.Name.IndexOf("toc.pdf", StringComparison.OrdinalIgnoreCase) > -1)
-                        continue;
-
-                    returnLst.Add(file);
-                }
-            }
-
-            return returnLst;
-        }
-
-        /// <summary>
-        /// Gets the relative file path from the root directory
-        /// </summary>
-        /// <param name="file"></param>
-        /// <returns></returns>
-        private String GetFileRelativePathFromDir (FileInfo file, DirectoryInfo dir)
-        {
-            // Check we have a selected dir and it exists
-            if (dir.Exists)
-            {
-                // Check the path ends with \ to make sure it is properly removed from the file
-                var dirPath = dir.FullName;
-                if (!dirPath.EndsWith("\\"))
-                    dirPath += "\\";
-                // If the file exists then replace the dir path with empty string to make it relative
-                if (file.Exists && file.FullName.IndexOf(dirPath, StringComparison.OrdinalIgnoreCase) > -1)
-                {
-                    var relative = file.FullName.Replace(dirPath, "");
-                    return relative.Replace('\\', '/');
-                }
-            }
-
-            return null;
-        }
-
-        /// <summary>
-        /// Gets the depth of the current file within the selected directory
-        /// </summary>
-        /// <param name="file"></param>
-        /// <returns></returns>
-        private Int32 GetFileDepthFromDir(FileInfo file, DirectoryInfo dir)
-        {
-            // Get the relative path
-            var relativePath = GetFileRelativePathFromDir(file, dir);
-
-            // Check it isn't null in case the file ges missing
-            if (!String.IsNullOrWhiteSpace(relativePath))
-            {
-                // Split and count based on the number of \ for the depth
-                return relativePath.Split('\\').Length - 1;
-            }
-
-            // Return 0 otherwise
-            return 0;
-        }
-
-        private void BuildPdfForDir(DirectoryInfo dir, Boolean coreTechnicalDocument)
-        {
-            if (dir.Exists)
-            {
-                var doc = new XmlDocument();
-                var htmlNode = doc.CreateElement("html");
-                doc.AppendChild(htmlNode);
-
-                var headNode = doc.CreateElement("head");
-                htmlNode.AppendChild(headNode);
-
-                if (File.Exists("Style/pdf-style.css"))
-                {
-                    var styleNode = doc.CreateElement("style");
-                    headNode.AppendChild(styleNode);
-
-                    var stylesNode = doc.CreateComment(File.ReadAllText("Style/pdf-style.css"));
-                    styleNode.AppendChild(stylesNode);
-                }
-
-                var bodyNode = doc.CreateElement("body");
-                htmlNode.AppendChild(bodyNode);
-
-                var ulNode = doc.CreateElement("ul");
-                bodyNode.AppendChild(ulNode);
-
-                if (coreTechnicalDocument)
-                {
-                    foreach (var subDir in dir.GetDirectories())
-                    {
-                        foreach (var file in subDir.GetFiles("*.pdf"))
-                        {
-                            BuildListItemForFile(file, dir, ulNode);
-                        }
-                    }
-                }
-                else
-                {
-                    foreach (var subDir in dir.GetDirectories())
-                    {
-                        BuildListForDir(subDir, dir, ulNode, coreTechnicalDocument);
-                    }
-
-                    foreach (var file in GetValidPdfsInDirectory(dir, SearchOption.TopDirectoryOnly))
-                    {
-                        BuildListItemForFile(file, dir, ulNode);
-                    }
-                }
-
-                var filename = String.Format("{0}-toc", coreTechnicalDocument ? "ctd" : dir.Name.ToLowerInvariant());
-
-                // doc.Save(Path.Combine(dir.FullName, filename + ".html"));
-
-                HtmlToPdf(doc, Path.Combine(dir.FullName, filename + ".pdf"));
-            }
-        }
-
-        private void HtmlToPdf(XmlDocument doc, String savePath)
-        {
-            using (MemoryStream msOutput = new MemoryStream())
-            {
-                using (TextReader reader = new StringReader(doc.OuterXml))
-                {
-                    using (Document document = new Document(PageSize.A4, 30, 30, 30, 30))
-                    {
-                        document.AddCreationDate();
-
-                        PdfWriter writer = PdfWriter.GetInstance(document, msOutput);
-
-                        document.Open();
-
-                        XMLWorkerHelper.GetInstance().ParseXHtml(
-                            writer,
-                            document,
-                            reader);
-
-
-
-                        document.Close();
-                    }
-                }
-
-                byte[] buffer = msOutput.ToArray();
-                // SOURCE: http://stackoverflow.com/questions/8140339/using-itextsharp-to-extract-and-update-links-in-an-existing-pdf
-                using (var reader = new PdfReader(buffer))
-                {
-                    for (var i = 1; i <= reader.NumberOfPages; i++)
-                    {
-                        var page = reader.GetPageN(i);
-                        var annots = page.GetAsArray(PdfName.ANNOTS);
-
-                        //Make sure we have something
-                        if ((annots == null) || (annots.Length == 0))
-                            continue;
-
-                        //Loop through each annotation
-                        foreach (PdfObject annot in annots.ArrayList)
-                        {
-                            //Convert the itext-specific object as a generic PDF object
-                            PdfDictionary AnnotationDictionary = (PdfDictionary)PdfReader.GetPdfObject(annot);
-
-                            //Make sure this annotation has a link
-                            if (!AnnotationDictionary.Get(PdfName.SUBTYPE).Equals(PdfName.LINK))
-                                continue;
-
-                            //Make sure this annotation has an ACTION
-                            // if (AnnotationDictionary.Get(PdfName.A) != null)
-                            //     continue;
-
-                            //Get the ACTION for the current annotation
-                            PdfDictionary AnnotationAction = (PdfDictionary)AnnotationDictionary.Get(PdfName.A);
-
-                            var uriObj = AnnotationAction.Get(PdfName.URI);
-
-                            if (uriObj != null && uriObj.IsString()) 
-                            {
-                                var uri = uriObj.ToString();
-                                if (!String.IsNullOrWhiteSpace(uri) && uri.IndexOf(".pdf") >= 1) 
-                                {
-                                    uri = uri.Replace('/', '\\');
-
-                                    AnnotationAction.Remove(PdfName.URI);
-                                    AnnotationAction.Put(PdfName.S, PdfName.GOTOR);
-                                    AnnotationAction.Put(PdfName.F, new PdfString(uri));
-                                }
-                            }
-                        }
-                    }
-
-                    //Next we create a new document add import each page from the reader above
-                    using (FileStream FS = new FileStream(savePath, FileMode.Create, FileAccess.Write, FileShare.None))
-                    {
-                        using (Document Doc = new Document())
-                        {
-                            using (PdfCopy writer = new PdfCopy(Doc, FS))
-                            {
-                                Doc.Open();
-                                for (int i = 1; i <= reader.NumberOfPages; i++)
-                                {
-                                    writer.AddPage(writer.GetImportedPage(reader, i));
-                                }
-                                Doc.Close();
-                            }
-                        }
-                    }
-                    
-
-                    reader.Close();
-                }
-            }
-        }
-
-        private void BuildListForDir(DirectoryInfo dir, DirectoryInfo rootDir, XmlNode node, Boolean coreTechnicalDocument)
-        {
-            var li = node.OwnerDocument.CreateElement("li");
-            node.AppendChild(li);
-
-            var span = node.OwnerDocument.CreateElement("span");
-            span.InnerText = dir.Name;
-            li.AppendChild(span);
-
-            var ulNode = node.OwnerDocument.CreateElement("ul");
-            li.AppendChild(ulNode);
-
-            if (!coreTechnicalDocument)
-            {
-                foreach (var subDir in dir.GetDirectories())
-                {
-                    BuildListForDir(subDir, rootDir, ulNode, coreTechnicalDocument);
-                }
-            }
-
-            foreach (var file in GetValidPdfsInDirectory(dir, SearchOption.TopDirectoryOnly))
-            {
-                BuildListItemForFile(file, rootDir, ulNode);
-            }
-        }
-
-        private void BuildListItemForFile(FileInfo file, DirectoryInfo rootDir, XmlNode node)
-        {
-            var li = node.OwnerDocument.CreateElement("li");
-            node.AppendChild(li);
-
-            var a = node.OwnerDocument.CreateElement("a");
-            a.SetAttribute("href", GetFileRelativePathFromDir(file, rootDir));
-            a.InnerText = file.Name;
-            li.AppendChild(a);
+            btn_Build.Enabled = _builder.ValidDir;
         }
 
         private void Builder_Load(object sender, EventArgs e)
---
+++ Submission TOC Builder/Builders/IBuilder.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace Submission_TOC_Builder.Builders
+{
+    /// <summary>
+    /// Interface class for builders should more be needed in future
+    /// </summary>
+    interface IBuilder
+    {
+        /// <summary>
+        /// Sets the current directory for the builder
+        /// </summary>
+        /// <param name="path"></param>
+        void SetDir(String path);
+        /// <summary>
+        /// Sets the current directory for the builder
+        /// </summary>
+        /// <param name="dir"></param>
+        void SetDir(DirectoryInfo dir);
+
+        /// <summary>
+        /// The number of files found that will be processed by the builder
+        /// </summary>
+        Int32 FoundFiles { get; }
+
+        /// <summary>
+        /// Is the directory OK and ready to work with
+        /// </summary>
+        Boolean ValidDir { get; }
+
+        /// <summary>
+        /// Performs the build
+        /// </summary>
+        /// <returns></returns>
+        Boolean Build();
+    }
+
+    /// <summary>
+    /// Abstract base class for builders to provide them base functionality
+    /// </summary>
+    public abstract class ABuilder : IBuilder
+    {
+        /// <summary>
+        /// The current directory the builder should work in
+        /// </summary>
+        protected DirectoryInfo _dir { get; set; }
+        public void SetDir(string path)
+        {
+            if (!String.IsNullOrWhiteSpace(path))
+            {
+                _dir = new DirectoryInfo(path);
+            }
+        }
+
+        public void SetDir(DirectoryInfo dir)
+        {
+            _dir = dir;
+        }
+
+        public abstract Int32 FoundFiles { get; }
+        public virtual Boolean ValidDir
+        {
+            get
+            {
+                return (_dir != null && _dir.Exists);
+            }
+        }
+
+        public abstract Boolean Build();
+    }
+}
